ef878c12e1c5a387d4a35faff16a857148a84722
Paulo Vieira <paullocostta86@gmail.com>
2021-04-16 00:55:46 -0300

    feat: finish challenge

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,33 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return;
+
+    const task: Task = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    };
+
+    setTasks(oldState => [...oldState, task]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task =>  task.id == id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task)
+
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const taskIndex = tasks.findIndex(task => task.id == id);
+    tasks.splice(taskIndex, 1);
+    setTasks (tasks.filter(
+      task => task.id !== id
+    ))
   }
 
   return (
